1c8f7c55bd086ac2c5647722b25ea54870149b66
ilayda yalçın <146716704+ilaydayacin@users.noreply.github.com>
2023-11-19 23:12:42 +0300

    Add files via upload

---
+++ 3 sayıdan büyük küçük/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace _3_sayıdan_büyük_küçük
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHesap = new System.Windows.Forms.Button();
+            this.txtS1 = new System.Windows.Forms.TextBox();
+            this.txtS2 = new System.Windows.Forms.TextBox();
+            this.txtS3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnHesap
+            // 
+            this.btnHesap.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesap.Location = new System.Drawing.Point(33, 186);
+            this.btnHesap.Name = "btnHesap";
+            this.btnHesap.Size = new System.Drawing.Size(161, 87);
+            this.btnHesap.TabIndex = 0;
+            this.btnHesap.Text = "Sonucu bul";
+            this.btnHesap.UseVisualStyleBackColor = true;
+            this.btnHesap.Click += new System.EventHandler(this.btnHesap_Click);
+            // 
+            // txtS1
+            // 
+            this.txtS1.Location = new System.Drawing.Point(124, 40);
+            this.txtS1.Name = "txtS1";
+            this.txtS1.Size = new System.Drawing.Size(166, 20);
+            this.txtS1.TabIndex = 1;
+            // 
+            // txtS2
+            // 
+            this.txtS2.Location = new System.Drawing.Point(124, 82);
+            this.txtS2.Name = "txtS2";
+            this.txtS2.Size = new System.Drawing.Size(166, 20);
+            this.txtS2.TabIndex = 2;
+            // 
+            // txtS3
+            // 
+            this.txtS3.Location = new System.Drawing.Point(124, 128);
+            this.txtS3.Name = "txtS3";
+            this.txtS3.Size = new System.Drawing.Size(166, 20);
+            this.txtS3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(28, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "1. Sayı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(28, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "2. Sayı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(28, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "3. Sayı";
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(28, 307);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(85, 25);
+            this.lblSonuc.TabIndex = 7;
+            this.lblSonuc.Text = "Sonuç: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 406);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtS3);
+            this.Controls.Add(this.txtS2);
+            this.Controls.Add(this.txtS1);
+            this.Controls.Add(this.btnHesap);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHesap;
+        private System.Windows.Forms.TextBox txtS1;
+        private System.Windows.Forms.TextBox txtS2;
+        private System.Windows.Forms.TextBox txtS3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblSonuc;
+    }
+}
+
---
+++ 3 sayıdan büyük küçük/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3_sayıdan_büyük_küçük
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesap_Click(object sender, EventArgs e)
+        {
+            int sayi1, sayi2, sayi3;
+            sayi1 = Convert.ToInt32(txtS1.Text);
+            sayi2 = Convert.ToInt32(txtS2.Text);
+            sayi3 = Convert.ToInt32(txtS3.Text);
+
+            if (sayi1 > sayi2 && sayi1 > sayi3)
+            {
+                lblSonuc.Text = "Sonuç: 1. Sayı daha büyük";
+            }
+            else if (sayi2 > sayi1 && sayi2 > sayi3)
+            {
+                lblSonuc.Text = "Sonuç: 2. Sayı daha büyük";
+            }
+            else
+            {
+                lblSonuc.Text = "Sonuç: 3. Sayı daha büyük";
+            }
+        }
+    }
+}
